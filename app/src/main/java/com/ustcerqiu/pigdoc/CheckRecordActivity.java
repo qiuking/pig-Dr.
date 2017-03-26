0e16d946695fffc36f7ef5df8e6db779ea7ab333
qiuking <qiuking@foxmail.com>
2017-03-26 18:03:05 +0800

    add resource files

--- app/src/main/java/com/ustcerqiu/pigdoc/CheckRecordActivity.java
+++ app/src/main/java/com/ustcerqiu/pigdoc/CheckRecordActivity.java
@@ -1,11 +1,18 @@
 package com.ustcerqiu.pigdoc;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Display;
+import android.view.Gravity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ArrayAdapter;
+import android.widget.NumberPicker;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -13,7 +20,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 //this activity deals with the intents of check the record date
-public class CheckRecordActivity extends BaseMinorClass implements View.OnClickListener {
+public class CheckRecordActivity extends BaseMinorClass implements View.OnClickListener,
+        NumberPicker.OnValueChangeListener, NumberPicker.OnScrollListener, NumberPicker.Formatter {
     //属性区域
     //
     //
@@ -66,9 +74,59 @@ public class CheckRecordActivity extends BaseMinorClass implements View.OnClickL
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, spinnerList);
         spinner.setAdapter(adapter);
 
+        Dialog dialog = new Dialog(this, R.style.mDialogTheme); // theme
+        dialog.setContentView(R.layout.alert_dialog_1);
+        //dialog.setTitle("wokao");
+        dialog.show();
+        Window dialogWindow = dialog.getWindow();
+        WindowManager.LayoutParams layoutParams = dialogWindow.getAttributes();
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        layoutParams.width = dm.widthPixels;
+        dialogWindow.setAttributes(layoutParams);
+        dialogWindow.getDecorView().setPadding(0,0,0,0); //占满控件
+        dialogWindow.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.BOTTOM); //布局位置
+
+        NumberPicker valuepicker = (NumberPicker) dialogWindow.findViewById(R.id.num_picker);  //注意在dialogWindow中找，否则找不到出错
+        String[] city = {"  立水桥立水桥立水桥立水桥立水桥立水桥  ","  霍营立水桥立水桥  ","  回龙观立水桥立水桥  ","  龙泽立水桥  ","  西二旗立水桥  ","  上地立水桥  "};
+        valuepicker.setFormatter(this);
+        valuepicker.setOnValueChangedListener(this);
+        valuepicker.setOnScrollListener(this);
+        valuepicker.setDisplayedValues(city);
+        valuepicker.setMinValue(0);
+        valuepicker.setMaxValue(city.length-1);
+        valuepicker.setValue(4);
+        //valuepicker.setWrapSelectorWheel(false);  //是否循环
+        valuepicker.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS); //避免点击后可修改状态
+
+
 
         //setOnClickListener (whole page)
         (findViewById(R.id.button_back)).setOnClickListener(this);
 
     }//onCreate
+
+    @Override //Formatter
+    public String format(int value) {
+        return "0"+value;
+    }
+
+    @Override //OnScrollListener
+    public void onScrollStateChange(NumberPicker view, int scrollState) {
+        switch(scrollState){
+            case NumberPicker.OnScrollListener.SCROLL_STATE_FLING: //滑动惯性过程种
+                break;
+            case NumberPicker.OnScrollListener.SCROLL_STATE_IDLE: //没滑动
+                break;
+            case NumberPicker.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL: //触摸滑动中
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override //OnValueChangeListener
+    public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+        //TODO
+    }
 }//end
