c7e5458808d3d029a5ee3d41df81ac401202bc60
qiuking <qiuking@foxmail.com>
2017-03-26 23:38:12 +0800

    add pig card RecycleView

--- app/src/main/java/com/ustcerqiu/pigdoc/CheckRecordActivity.java
+++ app/src/main/java/com/ustcerqiu/pigdoc/CheckRecordActivity.java
@@ -4,6 +4,8 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Display;
@@ -12,6 +14,7 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.NumberPicker;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -53,10 +56,10 @@ public class CheckRecordActivity extends BaseMinorClass implements View.OnClickL
 
     //define the data class and the RecycleView adapter for the pig information card outlines.
     //the data needed
-    public class pigCard{
+    public class PigCard{
         String number, sex, type, numYear, birthday, house, status, otherInfo;
-        //public pigCard(){ } //是否有默认无参数构造函数
-        public pigCard(String number, String sex, String type, String numYear, String birthday, String house, String status, String otherInfo){
+        public PigCard(){ } //是否有默认无参数构造函数. 不设置就无
+        public PigCard(String number, String sex, String type, String numYear, String birthday, String house, String status, String otherInfo){
             this.number = number;
             this.sex = sex;
             this.type = type;
@@ -67,20 +70,70 @@ public class CheckRecordActivity extends BaseMinorClass implements View.OnClickL
             this.otherInfo = otherInfo;
         }
     }//pigCard
-/*
+
     //the adapter for the pigCard list
-    public class pigCardAadpter extends mCom.comListAdapter<pigCard>{
+    public class PigCardAadpter extends mCom.comListAdapter<PigCard>{
         // the private List<T> mItemList 和 private int itemLayoutId  is in the parent；注意在构造函数中 itemLayoutId的赋值
         //unKnown the number of the holders, so use the list to store the listview holder content.
+        private List<TextView> numberList = new ArrayList<>();
+        private List<TextView> sexList = new ArrayList<>();
+        private List<TextView> typeList = new ArrayList<>();
+        private List<TextView> numYearList = new ArrayList<>();
+        private List<TextView> birthdayList = new ArrayList<>();
+        private List<TextView> houseList = new ArrayList<>();
+        private List<TextView> statusList = new ArrayList<>();
+        private List<TextView> otherInfoList = new ArrayList<>();
 
         //////////////////////////////////////
         //构造方法. set the data and the layout
-        public pigCardAadpter(List<pigCard> pigCardList, int pigCardLayoutId){
+        // R.layout.pig_card_outline_item
+        public PigCardAadpter(List<PigCard> pigCardList, int pigCardLayoutId){
             super(pigCardList, pigCardLayoutId);
+        }
+
+        @Override
+        public void setOnCreateViewHolder(mCom.ViewHolder holder, View v) {
+            numberList.add( (TextView) v.findViewById(R.id.pig_number_text) );
+            sexList.add( (TextView) v.findViewById(R.id.pig_sex) );
+            typeList.add( (TextView) v.findViewById(R.id.pig_type) );
+            numYearList.add( (TextView) v.findViewById(R.id.pig_num_year) );
+            birthdayList.add( (TextView) v.findViewById(R.id.pig_birthday) );
+            houseList.add( (TextView) v.findViewById(R.id.pig_house) );
+            statusList.add( (TextView) v.findViewById(R.id.pig_status) );
+            otherInfoList.add( (TextView) v.findViewById(R.id.pig_info_other) );
+            //TODO onclick action, and so on.
+        }
+
+        @Override
+        public void setOnBindViewHolder(mCom.ViewHolder holder, int position, PigCard item) {
+            int viewHolderPositon = holder.getInitPosition(); //this position record the view tag position
+            //set data to view in holder
+            numberList.get(viewHolderPositon).setText(item.number);
+            sexList.get(viewHolderPositon).setText(item.sex);
+            typeList.get(viewHolderPositon).setText(item.type);
+            numYearList.get(viewHolderPositon).setText(item.numYear);
+            birthdayList.get(viewHolderPositon).setText(item.birthday);
+            houseList.get(viewHolderPositon).setText(item.house);
+            statusList.get(viewHolderPositon).setText(item.status);
+            otherInfoList.get(viewHolderPositon).setText(item.otherInfo);
         }
     }//pigCardAdapter
 
-*/
+
+    //method to get data need to generate the list
+    public List<PigCard> getPigCardList(){
+        //TODO 完成获取数据的方法
+        List<PigCard> pigCardList = new ArrayList<>();
+        PigCard pigCard; // = new PigCard();
+        for(int i=0; i<20; i++){
+            pigCard = new PigCard("SBx123"+i, "母猪", "大约克", "3胎", "2017-03-26", "LHY02栋0"+i*3, "怀孕待产","饲养员：王2晶晶");
+            pigCardList.add(pigCard);
+        }
+        return pigCardList;
+    }// getPigCarList
+
+
+
     ///--##############################################################################-----/////
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -94,6 +147,16 @@ public class CheckRecordActivity extends BaseMinorClass implements View.OnClickL
         }
         //设置标题行的显示标题，替换掉layout中的默认标题
         ((TextView) this.findViewById(R.id.title_name_textView)).setText(titleName);  //this仅仅是强调本活动的调用环境
+
+        //generate the pig cards list
+        RecyclerView cardParentRecycleView = (RecyclerView) findViewById(R.id.pig_cards_list_RecyclerView);
+        List<PigCard> pigCardList = getPigCardList(); // get the data of the pig card
+        PigCardAadpter adpter = new PigCardAadpter(pigCardList, R.layout.pig_card_outline_item);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        cardParentRecycleView.setLayoutManager(layoutManager); //set layout parameters
+        cardParentRecycleView.setAdapter(adpter);
+
 
      /*   //spinner selector
         List<String> spinnerList = new ArrayList<>();
@@ -133,7 +196,18 @@ public class CheckRecordActivity extends BaseMinorClass implements View.OnClickL
         //setOnClickListener (whole page)
         (findViewById(R.id.button_back)).setOnClickListener(this);
 
+
+
+
+
+
+
+
     }//onCreate
+
+
+
+
 
     @Override //Formatter
     public String format(int value) {
